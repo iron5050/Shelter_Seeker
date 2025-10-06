0564d89ac8b10b9ed5a03b42aa6f74d70475f4bd
iron5050 <benedikt.ingi.a@gmail.com>
2025-10-06 13:46:22 +0000

    all dialouge

---
+++ Assets/Scripts/ConversationStarterChiken.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using DialogueEditor;
+
+public class ConversationStarterChiken : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("script is running");
+    }
+
+    [SerializeField] private NPCConversation ChikenConversation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E was pressed");
+                ConversationManager.Instance.StartConversation(ChikenConversation);
+            }
+        }
+    }
+}
+
